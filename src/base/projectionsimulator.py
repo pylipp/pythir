4454b1149e6bd09a3489605969acb5211e59776e
pylipp <philipp.metzner@tum.de>
2015-05-19 22:32:34 +0200

    Changes due to introduction of SystemMatrixEvaluator. Some refactoring.

--- src/base/projectionsimulator.py
+++ src/base/projectionsimulator.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python 
 
 from projections import Projections
-from systemmatrix import SystemMatrix
 
 class ProjectionSimulator(object):
     """
@@ -12,13 +11,11 @@ class ProjectionSimulator(object):
     If the reconstruction is performed using a binary system matrix, that
     matrix can be evaluated during the projection. 
     """
-    #FIXME The system matrix evaluation should be handled separately, with an
-    #accordingly sampled phantom
-
-    def __init__(self, phantom=None):
+    def __init__(self, views, nrBins, phantom=None):
         self.__phantom = phantom 
+        self.__nrBins = nrBins
+        self.__views = views
         self.__projections = None
-        self.__systemMatrix = None
 
     @property 
     def phantom(self):
@@ -28,33 +25,21 @@ class ProjectionSimulator(object):
     def projections(self):
         return self.__projections
 
-    @property 
-    def systemMatrix(self):
-        return self.__systemMatrix
+    def initProjections(self):
+        self.__projections = Projections(self.__views, self.__nrBins)
 
-    def initProjections(self, nrBins, views):
-        self.__projections = Projections(views, nrBins)
-
-    def initSystemMatrix(self, size=None, views=None):
-        #FIXME phantom needs to be shrinked
-        self.__systemMatrix = SystemMatrix((views,size,size))
-
-    def projectAll(self, nrBins, start, stop, views):
-        if self.phantom is None or self.projections is None or \
-                self.systemMatrix is None:
+    def projectAll(self, start, stop):
+        if self.phantom is None or self.projections is None: 
             return 
-        if nrBins != self.phantom.size:
+        if self.__nrBins != self.phantom.size:
             pass #resize phantom
 
         #project first view
         self.__projections.project(0, self.phantom)
-        #FIXME this is not correct. adjust add() or data to be added
-        self.__systemMatrix.add(0, self.phantom.data > 0.01)
-
+        views = self.__views
         angleInc = (stop-start)/float(views)
         for a in range(1, views):
             if a%25==0:
                 print "Projecting view " + str(a)
             rotPhantom = self.phantom.rotate(-a*angleInc)
-            self.__systemMatrix.add(a, rotPhantom > 0)
             self.__projections.project(a, rotPhantom)
