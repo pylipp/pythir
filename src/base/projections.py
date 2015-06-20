65e13ffb007d6718dba784f000ca710357899b32
pylipp <philipp.metzner@tum.de>
2015-06-20 23:14:03 +0200

    Projections are now distinguished by a data type. Introduced because algebraic reconstruction techniques use line integral values for computation but statistical ones (like maximum likelihood) use photon counts (i.e. N*exp(-l), l being the line integral, N blank scan photon counts).

--- src/base/projections.py
+++ src/base/projections.py
@@ -1,21 +1,31 @@
 import numpy as np 
 from phantom import Phantom
+from . import enum
 
 class Projections(object):
     """
     Represents all projections available to reconstruction. 
-    Holds data container with line integral values. 
+    Holds data container with a certain data type (either representing
+    attenuation line integrals or photon counts). 
     A 2D plot of this container yields the projection sinogram.
     """
-    def __init__(self, views, nrBins):
+
+    DataType = enum(
+            LINE_INTEGRAL=0,
+            PHOTON_COUNT=1 )
+
+    def __init__(self, views, nrBins, dataType=DataType.LINE_INTEGRAL):
         """
         :param      views | int 
                     nrBins | int 
+                    dataType | Projections.DataType
         :attrib     __data | numpy.2darray
         """
         self.__views = views 
         self.__nrBins = nrBins 
         self.__data = np.zeros((views, nrBins))
+        self._dataType = dataType
+        self._maxPhotons = 100000
 
     @property 
     def nrBins(self):
@@ -42,6 +52,20 @@ class Projections(object):
         """ :return     int """
         return self.__views
 
+    def convertDataType(self):
+        """
+        Converts the projection data to line integral values if the current
+        data type is PHOTON_COUNT and vice versa.
+        """
+        if self._dataType == Projections.DataType.LINE_INTEGRAL:
+            self.__data[:,:] = self._maxPhotons * np.exp( - self.data[:,:] )
+            self._dataType = Projections.DataType.PHOTON_COUNT
+        elif self._dataType == Projections.DataType.PHOTON_COUNT:
+            # assert positive arguments for logarithm
+            self.__data[self.data <= 0] = 1.e-9
+            self.__data[:,:] = np.log( self._maxPhotons / self.data[:,:] )
+            self._dataType = Projections.DataType.LINE_INTEGRAL
+
     def project(self, view, phantom):
         """
         Adds up each column of the given phantom and writes the sum into the
@@ -53,3 +77,5 @@ class Projections(object):
         if isinstance(phantom, Phantom):
             phantom = phantom.data
         self.__data[view, :] = np.sum(phantom, axis=0)
+        if self._dataType == Projections.DataType.PHOTON_COUNT:
+            self.__data[view, :] = self._maxPhotons * np.exp( - self.__data[view, :] )
